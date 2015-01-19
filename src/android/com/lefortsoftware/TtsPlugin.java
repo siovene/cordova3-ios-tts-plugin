32a24a79be0b31223951e0c6eb93696896c95a33
Salvatore Iovene <salvatore@iovene.com>
2015-01-19 22:22:31 +0200

    Indent Android file, remove hardcoded French locale, fix logic.

--- src/android/com/lefortsoftware/TtsPlugin.java
+++ src/android/com/lefortsoftware/TtsPlugin.java
@@ -1,76 +1,56 @@
-    package com.lefortsoftware.ttsplugin;
+package com.lefortsoftware.ttsplugin;
 
-        import org.apache.cordova.CallbackContext;
-        import org.apache.cordova.CordovaPlugin;
-        import org.json.JSONArray;
-        import org.json.JSONException;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-        import android.speech.tts.TextToSpeech;
-        import java.util.Locale;
+import android.speech.tts.TextToSpeech;
+import java.util.Locale;
 
-        import android.widget.Toast;
+import android.widget.Toast;
 
-        public class TtsPlugin extends CordovaPlugin{
+public class TtsPlugin extends CordovaPlugin {
+    TextToSpeech tts;
+    CallbackContext cbc;
 
-        TextToSpeech tts;
-        CallbackContext cbc;
-
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
+    @Override
+    public boolean execute(
+        String action, JSONArray args,
+        CallbackContext callbackContext) throws JSONException
+    {
         cbc = callbackContext;
 
-        if(action.equals("echo")){
-        String message = args.getString(0);
-        callbackContext.success(message);
-        return true;
+        if(action.equals("initTTS")) {
+            tts = new TextToSpeech(
+                cordova.getActivity().getApplicationContext(),
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status == TextToSpeech.SUCCESS) {
+                            cbc.success("TextToSpeech initialized");
+                        } else {
+                            cbc.error("Could not initialize TextToSpeech");
+                        }
+                    }
+                }
+            );
+        } else if(action.equals("speak")) {
+            String txt = args.getString(0);
+            tts.speak(txt, TextToSpeech.QUEUE_ADD, null);
+        } else if(action.equals("stop")) {
+            tts.stop();
+        } else if(action.equals("setLanguage")){
+            // Not implemented
+            return false;
+        } else if(action.equals("setRate")){
+            // Not implemented
+            return false;
+        } else {
+            return false;
         }
 
-        if(action.equals("initTTS")){
-        tts = new TextToSpeech(cordova.getActivity().getApplicationContext(), new TextToSpeech.OnInitListener()
-        {
-        @Override
-        public void onInit(int status) {
-        if(status == TextToSpeech.SUCCESS)
-        {
-        //Toast.makeText(cordova.getActivity().getApplicationContext(), "Init OK", Toast.LENGTH_LONG).show();
-        cbc.success();
-        }
-        else
-        {
-        //Toast.makeText(cordova.getActivity().getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
-        cbc.error("ERROR");
-        }
-        }
-        } );
+        cbc.success("OK");
         return true;
-        }
-
-        if(action.equals("speak")){
-        String txt = args.getString(0);
-        tts.setLanguage(Locale.FRANCE);
-        tts.speak(txt, TextToSpeech.QUEUE_ADD, null);
-        callbackContext.success("OK");
-        return true;
-        }
-
-        if(action.equals("stop")){
-        tts.stop();
-        callbackContext.success("OK");
-        return true;
-        }
-
-        if(action.equals("setLanguage")){
-        callbackContext.success("OK");
-        return true;
-        }
-
-        if(action.equals("setRate")){
-        callbackContext.success("OK");
-        return true;
-        }
-
-        callbackContext.success(action);
-        return true;
-        }
-        }
+    }
+}
